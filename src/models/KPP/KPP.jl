867bb6bc887ab3af4de04ac3c874b5aa61f6c30c
sandreza <andrenogueirasouza@gmail.com>
2019-12-05 19:12:32 -0500

    changed what the parameters mean in flexible KPP

--- src/models/KPP/KPP.jl
+++ src/models/KPP/KPP.jl
@@ -220,7 +220,7 @@ i is a face index.
     w1 = max(0, h * Qb)^(1/3)
     w2 = sqrt(N² * h^2)
     tmp = CKE * w1 * w2 + CKE₀
-    tmp += CKE2 * w1^(2.0-CKE3) * w2^CKE3
+    tmp += CKE2 * w1 * h #CKE has to have units of N
     return tmp
 end
 
